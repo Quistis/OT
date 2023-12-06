7ebf41e7a425de03b7b268fbf3857bfc41c76aef
Quistis <SuicideXhelia@gmail.com>
2023-12-06 16:18:59 +0500

    Удалил лишние переносы

--- js/filters.js
+++ js/filters.js
@@ -70,8 +70,6 @@ const onFiltersListClick = (evt) => {
       return;
     }
 
-
-
     const url = `http://127.0.0.1:8000/employees/get_all_with_exams?division=${targetId}`;
 
     sendRequest(onDataSuccess, url);
