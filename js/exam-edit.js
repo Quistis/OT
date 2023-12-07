d56e24909be6ff419ddfb66eb2e6869f73ce6faf
Sarbassov <bagdat250716@gmail.com>
2023-12-07 13:53:52 +0500

    добавлена возможность редактирования и добавления типа экзамена

--- js/exam-edit.js
+++ js/exam-edit.js
@@ -2,65 +2,93 @@ const editForm = document.querySelector('.exam-type-form');
 const editName = editForm.querySelector('.edit-name');
 const editPeriod = editForm.querySelector('.edit-period');
 const editDescription = editForm.querySelector('.edit-description');
-
-const editButtons = document.querySelectorAll('.edit-exam-type-btn');
-
-const onEditBtnClick = (btn) => {
-  //console.log(btn.dataset.examId);
-  const examRow = document.querySelector(`#exam-id-${btn.dataset.examId}`);
-
-  const examName = examRow.querySelector('.exam-type-name').textContent;
-  const examPeriod = examRow.querySelector('.exam-type-period').textContent;
-  const examDescription = examRow.querySelector('.exam-type-description').textContent;
-  //console.log(editName);
-  editName.value = examName;
-  editPeriod.value = examPeriod;
-  editDescription.textContent = examDescription;
-};
+const addExamType = document.querySelector('.add-exam-type-btn');
+const editButtons = document.querySelectorAll('.edit-exam-type-btn')
+const closeButton = editForm.querySelector(".close-button");
 
 for (const btn of editButtons) {
-  btn.addEventListener('click', onEditBtnClick);
+  btn.addEventListener('click', () => {
+    const examRow = document.querySelector(`#exam-id-${btn.dataset.examId}`);
+
+    editForm.dataset.examId = btn.dataset.examId;
+
+    const examName = examRow.querySelector('.exam-type-name').textContent;
+    const examPeriod = examRow.querySelector('.exam-type-period').textContent;
+    const examDescription = examRow.querySelector('.exam-type-description').textContent;
+
+    editName.value = examName;
+    editPeriod.value = examPeriod;
+    editDescription.value = examDescription;
+
+  });
 }
+
+addExamType.addEventListener('click', () => {
+  
+  editForm.dataset.examId = null;
+  editName.value = null;
+  editPeriod.value = null;
+  editDescription.value = null;
+
+});
 
 editForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
-
+  
   const formData = new FormData(evt.target);
-  console.log(formData);
 
-
-  // fetch(
-  //   '/auth/jwt/login',
-  //   {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/x-www-form-urlencoded'
-  //     },
-  //     body: new URLSearchParams({
-  //     'username': formData.get("username"),
-  //     'password': formData.get("password")
-  //   })
-  //   },
-  // )
-  // .then((response) => {
-
-  //   if (!response.ok) {
-  //     loginError.style.display = 'block';
-  //     setTimeout(() => {
-  //       loginError.style.display = 'none';
-  //     }, 2000);
-  //     return 0;
-  //   };
-  //   location.href = '/';
-  // })
-  // .then((data) => {
-  //     console.log(data);
-  //     // if (data.detail === 'LOGIN_BAD_CREDENTIALS') {
-  //     //   alert('ERROR');
-  //     // };
-
-  //   })
-  // .catch((err) => {
-  //   console.log(err);
-  // });
+  let object = {};
+  formData.forEach((value, key) => object[key] = value);
+  
+  if (editForm.dataset.examId != null) {
+    fetch(
+      `/exam_types/update/${editForm.dataset.examId}`,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(object)
+      },
+    )
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data.id);
+  
+        const examRow = document.querySelector(`#exam-id-${data.id}`);
+  
+        const examName = examRow.querySelector('.exam-type-name');
+        const examPeriod = examRow.querySelector('.exam-type-period');
+        const examDescription = examRow.querySelector('.exam-type-description');
+  
+        examName.textContent = object.name;
+        examPeriod.textContent = object.period;
+        examDescription.textContent  = object.description;
+  
+        closeButton.click();
+  
+      })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+  else {
+    fetch(
+      `/exam_types/add`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(object)
+      },
+    )
+    .then((response) => response.json())
+    .then((data) => {
+        location = "/exam-types"
+      })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
 });
