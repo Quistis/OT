293d8ab9ab8fa272165139f6121a5ac8010a73bf
Sarbassov <bagdat250716@gmail.com>
2024-01-17 10:42:12 +0500

    реализовано изменение данных сотрудника на стринце после обновления данных на сервере

--- js/add-exam.js
+++ js/add-exam.js
@@ -54,14 +54,13 @@ function editEmployee(evt) {
 
   const formData = new FormData(evt.target);
   const editEmployeeForm = document.querySelector('.edit-employee-form');
-  let employee_id = editEmployeeForm.dataset.employeeId;
+  let employeeId = editEmployeeForm.dataset.employeeId;
 
   let object = {};
   formData.forEach((value, key) => object[key] = value);
-  console.log(object);  
 
   fetch(
-    `/employees/update/${employee_id}/`,
+    `/employees/update/${employeeId}/`,
     {
       method: 'PATCH',
       headers: {
@@ -72,30 +71,22 @@ function editEmployee(evt) {
   )
   .then((response) => response.json())
   .then((data) => {
-      // const collapse = document.getElementById(`${employee_id}`);
-      // const tableBody = collapse.querySelector('tbody');
-
       const closeButton = editEmployeeForm.querySelector(".close-button");
       closeButton.click();
       
-      // if (tableBody == null) {
-      //   const examinationsTableTemplate = document.querySelector('#examinations-table')
-      //   .content
-      //   .querySelector('.employee-examaminations');
-      //   const examinationsTableContainer = examinationsTableTemplate.cloneNode(true);
-      //   const cardBody = collapse.querySelector('.card-body');
-      //   cardBody.append(examinationsTableContainer);
-      //   const tableBody = collapse.querySelector('tbody');
+      const collapse = document.getElementById(`${employeeId}`);
 
-      //   const row = createExamination(data, 0);
-        
-      //   tableBody.append(row);
-      // }
-      // else {
-      //   const rows = tableBody.querySelectorAll("tr");
-      //   const row = createExamination(data, rows.length);
-      //   tableBody.append(row);
-      // };
+      const fio = document.querySelector(`#employee-name-${employeeId}`);
+      const position =  collapse.querySelector('.employee-position');
+      const subdivision = collapse.querySelector('.employee-subdivision');
+      const certificate = collapse.querySelector('.employee-certificate');
+      const division = collapse.querySelector('.employee-division');
+
+      fio.textContent = data.fio;
+      position.textContent = `Должность: ${data.position}`
+      subdivision.textContent = `Группа, отдел: ${data.subdivision}`
+      certificate.textContent = `Номер удостоверения: ${data.certificate}`
+      division.textContent =  `Подразделение: ${data.division}` 
     });
 
 }
